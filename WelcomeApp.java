20afaa29f15eedb317c64457855bcf74b130af2c
ranj786 <ranjitsoni2009@gmail.com>
2022-06-16 12:54:23 +0530

    First file added

---
+++ WelcomeApp.java
@@ -0,0 +1,13 @@
+
+public class WelcomeApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to for loop");
+		int i=1;
+		while(i<=10) {
+			System.out.println("Welcome to App : "+i);
+			++i;
+		}
+	}
+
+}
